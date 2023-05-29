161ca3115b7b9e3145ac06fd6bcb6692bbd025b9
Mist <marcus-le@hotmail.com>
2023-05-29 14:39:05 +0200

    Création Footer & Header

--- src/pages/Accueille/accueille.jsx
+++ src/pages/Accueille/accueille.jsx
@@ -1,9 +1,15 @@
+import Footer from "../../composant/Footer/footer"
+import Header from "../../composant/Header/header"
+
 const Accueille =()=>{
     return(
         <div>
+            <Header/>
             <main>
                 <h1>AZERTYUIOP</h1>
             </main>
+            <Footer/>
+
         </div>
     )
 }
